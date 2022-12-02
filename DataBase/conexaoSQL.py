469d9a0e5a20db0e0266c940f63f2a0d901b2b68
DiomarBR <diomarbr4@gmail.com>
2022-12-02 14:32:15 -0300

    update

--- DataBase/conexaoSQL.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtSql import QSqlDatabase
-
-class ConexaoSQL:
-    def getConexao():
-        db = QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName("produtos.db")
-
-        return db
--- View/main.py
+++ View/main.py
@@ -1,11 +1,23 @@
 from PyQt5 import uic,QtWidgets
+import sqlite3
 
-def VoltarInicio():
+def FecharEstoque():
     estoque.close()
     inicial.show()
+
+def FecharVendas():
+    vendas.close()
+    inicial.show()
+    
+def ChamarVendas():
+    vendas.show()
+    estoque.close()
+    inicial.close()
+    
     
 def ChamarEstoque():
     estoque.show()
+    vendas.close()
     inicial.close()
     
 
@@ -13,13 +25,37 @@ def ChamarEstoque():
     
 app=QtWidgets.QApplication([])
 inicial=uic.loadUi(r"View\Ui\maingui.ui")
+vendas=uic.loadUi(r"View\Ui\vendas.ui")
 estoque=uic.loadUi(r"View\Ui\produtos.ui")
 inicial.button_estoque.clicked.connect(ChamarEstoque)
-estoque.inicio.clicked.connect(VoltarInicio)
+inicial.button_vendas.clicked.connect(ChamarVendas)
+vendas.butto_main.clicked.connect(FecharVendas)
+vendas.button_estoque.clicked.connect(ChamarEstoque)
+estoque.inicio.clicked.connect(FecharEstoque)
+estoque.button_vendas.clicked.connect(ChamarVendas)
 
 inicial.show()
 app.exec()
-# import sqlite3
+
+
+def SalvarDados():
+    codigo1 = estoque.codigo01.text()
+    codigo2 = estoque.codigo02.text()
+    nome = estoque.nome.text()
+    valor = estoque.valor.text()
+    quantidade = estoque.quantidade.text()
+    
+    try:
+        banco = sqlite3.connect(r'DataBase\produtos.db')
+        cursor = banco.cursor()
+        cursor.execute(f"INSERT INTO estoque VALUES({codigo1}, {codigo2}, '{nome}', {valor}, {quantidade})")
+        banco.commit()
+    except sqlite3.Error as erro:
+        print('Erro ao inserir dados', erro)
+
+estoque.pushButton.clicked.connect(SalvarDados)
+
+
 # con = sqlite3.connect("aplicacao_de_venda_e_controle_de_estoque\DataBase\produtos.db")
 # codigo = int(input("Digite o codigo de barras: "))
 # codig2 = int(input("Digite o codigo de barras 2 se nao tem e 0: "))
@@ -29,8 +65,9 @@ app.exec()
 # cursor = con.cursor()
 # cursor.execute(f"INSERT INTO estoque VALUES({codigo}, {codig2}, '{nome}', {valor}, {quantidade})")
 # con.commit()
-#
+
 # x = cursor.execute("SELECT * FROM estoque")
-#
+
 # for i in x:
 #     print(i)
+
